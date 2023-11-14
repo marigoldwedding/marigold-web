e1d740c05158e642ed1c9143dd08fcd8db8c470e
marigoldwedding <marigoldwo@gmail.com>
2023-11-14 07:33:32 +0700

    update new UI nad new content

---
+++ js/owlcarousel.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $("#myCarousel").owlCarousel({
+      items: 3, // Number of cards shown in each slide
+      loop: true, // Continuously loop the carousel
+      margin: 20, // Space between cards
+      nav: true, // Show navigation buttons
+      navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"], // Custom navigation icons
+      responsive: {
+        0: {
+          items: 1 // Number of cards shown in the carousel for smaller screens
+        },
+        768: {
+          items: 2 // Number of cards shown in the carousel for medium screens
+        },
+        992: {
+          items: 3 // Number of cards shown in the carousel for large screens
+        }
+      }
+    });
+  });
